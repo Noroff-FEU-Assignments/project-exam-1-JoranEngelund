42235519a476ed99d43377a064c5ace4d73d4a18
JoranEngelund <joranengelund@hotmail.com>
2022-09-05 18:27:42 +0200

    updated api fetch in index.js so media comes trough api fetch

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const carousel = document.querySelector(".carousel");
 
-const url = "https://life-api.engelund.site/wp-json/wp/v2/media/";
+const url = "https://life-api.engelund.site/wp-json/wp/v2/posts?page=1&_embed";
+const secondUrl =
+  "https://life-api.engelund.site/wp-json/wp/v2/posts?page=2&_embed";
 
 async function fetchPosts() {
   try {
     const response = await fetch(url);
     const posts = await response.json();
     console.log(posts);
-
     posts.forEach(function (post) {
-      carousel.innerHTML += `<div>
-                                <h2>${post.title.rendered}</h2>
-                                <img src="${post.guid.rendered}">
-                                <div>${post.description.rendered}</div>
+      const blogImage = post._embedded?.["wp:featuredmedia"][0].source_url;
+      const blogImageAlt = post._embedded?.["wp:featuredmedia"][0].alt_text;
+      carousel.innerHTML += `<div class="slide">
+                              <img src="${blogImage}" alt="${blogImageAlt}"/>
+                              <h2>${post.title.rendered}</h2>
+                              <h3>${post.excerpt.rendered}</h3>
+                              <a href="/post-specific.html">View Post</a>
                             </div>`;
     });
   } catch (error) {
@@ -20,3 +24,15 @@ async function fetchPosts() {
   }
 }
 fetchPosts();
+
+async function secondaryPosts() {
+  try {
+    const response = await fetch(secondUrl);
+    const posts = await response.json();
+    console.log(posts);
+  } catch (error) {
+    console.log("This error is for secondary posts: " + error);
+  }
+}
+
+secondaryPosts();
