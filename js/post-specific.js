6693550ab797359cbb8a3d9a57c856696cc690a8
JoranEngelund <joranengelund@hotmail.com>
2022-09-08 02:33:08 +0200

    updated api url for post-specific.js

--- js/post-specific.js
+++ js/post-specific.js
@@ -2,7 +2,8 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-const url = "https://life-api.engelund.site/wp-json/wp/v2/posts/" + id;
+const url =
+  "https://life-api.engelund.site/wp-json/wp/v2/posts/" + id + "/?_embed";
 
 const detailContainer = document.querySelector(".post-specific");
 
@@ -13,16 +14,23 @@ async function fetchPostDetails() {
     console.log(details);
 
     document.title = `${details.title.rendered} | Life`;
+    const blogImage = details._embedded?.["wp:featuredmedia"][0].source_url;
+    const blogImageAlt = details._embedded?.["wp:featuredmedia"][0].alt_text;
 
-    detailContainer.innerHTML += `
+    detailContainer.innerHTML += `<div>
+                                    <img src="${blogImage}" alt="${blogImageAlt}" />
                                     <h1>${details.title.rendered}</h1>
-                                    ${details.content.rendered}
-                                    <div class="cta-container">
-                                <a class="cta" href="/posts.html">View other posts</a>
-                              </div>
-                                    `;
+                                    <div class="underline-headings"></div>
+                                    <h2>${details.date}</h2>
+                                    <div>${details.content.rendered}</div
+                                  </div>
+                                  <div class="underline-headings"></div>
+                                  <div class="latest-comments">
+                                  </div>
+                                  `;
   } catch (error) {
     console.log(error);
   }
 }
+
 fetchPostDetails();
