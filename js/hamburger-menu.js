da3d395460469694641901ef46cd94b651ccde6f
JoranEngelund <joranengelund@hotmail.com>
2022-09-21 17:03:32 +0200

    updated hamburgermenu function

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,20 +1,22 @@
 const hamburgerMenuOpen = document.querySelector(".hamburger-open");
 const hamburgerMenuClosed = document.querySelector(".hamburger-close");
+const openBtn = document.querySelector("openMenu");
 
-const openBtn = document.querySelector(".openMenu");
-const closeBtn = document.querySelector(".closeMenu");
 const navigation = document.querySelector("ul");
 
-hamburgerMenuOpen.addEventListener("click", (e) => {
-  navigation.style.display = "flex";
-  openBtn.style.display = "none";
-  hamburgerMenuClosed.style.display = "flex";
-  closeBtn.style.display = "flex";
-});
+function removeNav() {
+  const Screenwidth = window.innerWidth;
+  if (Screenwidth <= 764) {
+    navigation.style.display = "none";
+  }
+}
 
-hamburgerMenuClosed.addEventListener("click", (e) => {
-  navigation.style.display = "none";
-  openBtn.style.display = "flex";
-  hamburgerMenuClosed.style.display = "none";
-  closeBtn.style.display = "none";
+removeNav();
+
+hamburgerMenuOpen.addEventListener("click", (e) => {
+  if (navigation.style.display === "none") {
+    navigation.style.display = "flex";
+  } else {
+    navigation.style.display = "none";
+  }
 });
